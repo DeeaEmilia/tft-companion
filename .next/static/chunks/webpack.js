fb864e273a6ef1dea6be4b08a9ec291524c13f8d
Deea Emilia Duta <deeaemiliad@gmail.com>
2023-07-02 12:31:18 +0300

    fixed item page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "229c66a79eb366a9"; }
+/******/ 		__webpack_require__.h = function() { return "1466225c625f870f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
