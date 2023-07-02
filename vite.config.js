1499ca0a1d53f3f9c8db61af723cb07d00142deb
Deea Emilia Duta <deeaemiliad@gmail.com>
2023-07-02 08:27:10 +0300

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-// import { VitePluginSass } from 'vite-plugin-sass';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-        _redirects: path.resolve(__dirname, "_redirects"),
-      },
-    },
-  },
 });
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
