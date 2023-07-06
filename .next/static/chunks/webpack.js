fb561209c81197876a96237a5b3f19e21d360403
Deea Emilia Duta <deeaemiliad@gmail.com>
2023-07-06 11:49:32 +0300

    adjusted grayscale

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3cfe9b9ae8bb3655"; }
+/******/ 		__webpack_require__.h = function() { return "42514451f1158a8a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
